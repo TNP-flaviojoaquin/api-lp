e8400d5e78063de6140f0f53e4f4817f7a43adb1
Flavio Vittorino Joaquin Yanqui <flavio.joaquin@thinknetworks.pe>
2022-09-29 13:45:07 -0500

    your_commit_message

---
+++ saveChanges.bat
@@ -0,0 +1,10 @@
+@echo off
+
+cd C:\Users\flavio.joaquin\Music\Laboratorios\API REST - 27.09.22\api-lp
+
+git init .
+git add --all
+git commit -m "your_commit_message"
+git push origin master
+
+exit
